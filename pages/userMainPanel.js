8dc64de757bb458b08563a6e34b8cc5a31f826d4
OgaiDmitrii <105227968+OgaiDmitrii@users.noreply.github.com>
2022-05-19 13:00:37 +0500

    !

--- pages/userMainPanel.js
+++ pages/userMainPanel.js
@@ -23,7 +23,12 @@ import {
     import { useState } from 'react';
     import nookies from "nookies"
     import axios from 'axios';
-
+    import pict1 from "./img/MainMenu1.svg"
+    import pict2 from "./img/MainMenu2.png"
+    import pict3 from "./img/MainMenu3.png"
+    import pict4 from "./img/MainMenu4.png"
+    import pict5 from "./img/MainMenu5.png"
+    import pict6 from "./img/MainMenu6.png"
  
 
   export default function UserManualPanel(props) {
@@ -60,25 +65,73 @@ import {
       </style>
       <Flex direction="column" width="100vw" height="100vh" pt="10%">
           <Flex direction="row" height="43%" margin="1%">
-            <Box h="98%" w="31.3%" backgroundColor={"red.400"} margin="1%">
-                1
+            <Box h="98%" w="31.3%" backgroundColor={"#c3adb3"} margin="1%" _hover={{ bg: '#c3add6'}}>
+                <Box h="33%">
+                  <Text fontSize={18} color="white" padding={5}>1. BIO ОРГАНИЗАЦИИ ОБРАЗОВАНИЯ</Text>
+                </Box>
+                <Flex justifyContent={"center"} h={"136px"}>
+                  <Image src={pict1} quality={100}  width={"136px"} />
+                </Flex>
+                <Box h="33%" textAlign={"center"}>
+                  <Text fontSize={16} color="white" padding={5}>краткая информация об организации образования</Text>
+                </Box>
             </Box>
-            <Box h="98%" w="31.3%" backgroundColor={"orange.400"} margin="1%">
-                2
+            <Box h="98%" w="31.3%" backgroundColor={"#d09786"} margin="1%"  _hover={{ bg: '#d097a6'}}>
+                <Box h="33%">
+                  <Text fontSize={18} color="white" padding={5}>2. СОДЕРЖАНИЕ ОБРАЗОВАНИЯ С ОРИЕНТИРОМ  НА РЕЗУЛЬТАТ</Text>
+                </Box>
+                <Flex justifyContent={"center"} h={"136px"}>
+                  <Image src={pict2} quality={100}  width={"136px"} />
+                </Flex>
+                <Box h="33%" textAlign={"center"}>
+                  <Text fontSize={16} color="white" padding={5}>ГОСО, учебный план, учебная программа</Text>
+                </Box>
             </Box>
-            <Box h="98%" w="31.3%" backgroundColor={"yellow.400"} margin="1%">
-                3
+            <Box h="98%" w="31.3%" backgroundColor={"#b38f6a"} margin="1%"  _hover={{ bg: '#b38f8a'}}>
+                <Box h="33%">
+                  <Text fontSize={18} color="white" padding={5}>3. МАКСИМАЛЬНЫЙ ОБЪЕМ УЧЕБНОЙ НАГРУЗКИ ОБУЧАЮЩИХСЯ И ВОСПИТАННИКОВ </Text>
+                </Box>
+                <Flex justifyContent={"center"} h={"136px"}>
+                  <Image src={pict3} quality={100}  width={"136px"} />
+                </Flex>
+                <Box h="33%" textAlign={"center"}>
+                  <Text fontSize={16} color="white" padding={5}>ОУД, дисциплины, часы</Text>
+                </Box>
             </Box>
           </Flex>
           <Flex direction="row" height="43%"  margin="1%">
-          <Box height="98%" width="31.3%" backgroundColor={"green.400"} margin="1%">
-                1
+            <Box h="98%" w="31.3%" backgroundColor={"#B5C170"} margin="1%"  _hover={{ bg: '#B5C190'}}>
+                <Box h="33%">
+                  <Text fontSize={18} color="white" padding={5}>4. УРОВЕНЬ ПОДГОТОВКИ ВОСПИТАННИКОВ И ОБУЧАЮЩИХСЯ</Text>
+                </Box>
+                <Flex justifyContent={"center"} h={"136px"}>
+                  <Image src={pict4} quality={100}  width={"136px"} />
+                </Flex>
+                <Box h="33%" textAlign={"center"}>
+                  <Text fontSize={16} color="white" padding={5}>Диагностика/СОР/СОЧ/тестирование/анкетирование</Text>
+                </Box>
             </Box>
-            <Box height="98%" width="31.3%" backgroundColor={"cyan.400"} margin="1%">
-                2
+            <Box h="98%" w="31.3%" backgroundColor={"#5fbeb8"} margin="1%"  _hover={{ bg: '#5fbed8'}}>
+                <Box h="33%">
+                  <Text fontSize={18} color="white" padding={5}>5. СРОКИ ОБУЧЕНИЯ</Text>
+                </Box>
+                <Flex justifyContent={"center"} h={"136px"}>
+                  <Image src={pict5} quality={100}  width={"136px"} />
+                </Flex>
+                <Box h="33%" textAlign={"center"}>
+                  <Text fontSize={16} color="white" padding={5}>требования к продолжительности учебного года, к срокам освоения программы</Text>
+                </Box>
             </Box>
-            <Box height="98%" width="31.3%" backgroundColor={"blue.400"} margin="1%">
-                3
+            <Box h="98%" w="31.3%" backgroundColor={"#86a1a9"} margin="1%"  _hover={{ bg: '#86a1c9'}}>
+                <Box h="33%">
+                  <Text fontSize={18} color="white" padding={5}>6. ОТЧЕТ ПО САМООЦЕНКЕ </Text>
+                </Box>
+                <Flex justifyContent={"center"} h={"136px"}>
+                  <Image src={pict6} quality={100}  width={"136px"} />
+                </Flex>
+                <Box h="33%" textAlign={"center"}>
+                  <Text fontSize={16} color="white" padding={5}>заключение, протокол педсовета, приложения</Text>
+                </Box>
             </Box>
           </Flex>
       </Flex>
@@ -107,7 +160,7 @@ import {
         return {
             redirect: {
                 permanent: false,
-                destination: '//'
+                destination: '/'
             }
         }
     else
